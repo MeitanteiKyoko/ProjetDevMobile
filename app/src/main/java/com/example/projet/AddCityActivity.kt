b1240409775144bfbccd94d0c839275263e173d6
Caroline <caro.randria.sweety@gmail.com>
2020-11-16 21:52:53 +0100

    Projet V.1

---
+++ app/src/main/java/com/example/projet/AddCityActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddCityActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_city)
+    }
+}
